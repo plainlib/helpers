f102e8933bc2c00ff3c8801bfda3a677bbc53a9c
Alexander Tverskoy <astverskoy@gmail.com>
2026-09-03 15:35:19 +0300

    Linux compatibility update

--- controlshelper.pas
+++ controlshelper.pas
@@ -22,8 +22,10 @@ uses
   Graphics,
   ColorBox,
   CheckLst,
-  Math,
   Themes,
+  {$IFDEF WINDOWS}
+  Math,
+  {$ENDIF}
   LCLIntf,
   LCLType,
   LazUTF8,
@@ -242,59 +244,67 @@ begin
   begin
     // Get actual window rectangle including non-client area
     WinRect := Default(TRect);
-    if GetWindowRect(TForm(Self).Handle, WinRect) then
+    {$IFDEF WINDOWS}
+    if not GetWindowRect(TForm(Self).Handle, WinRect) then
+      Exit;
+    {$ELSE}
+    WinRect := TForm(Self).BoundsRect;
+    {$ENDIF}
+
+    // Select full desktop for stay-on-top forms, otherwise work area only
+    if TForm(Self).FormStyle = fsSystemStayOnTop then
+      LimitRect := Screen.DesktopRect
+    else
+      LimitRect := Screen.WorkAreaRect;
+
+    // Check if form is larger than available screen area
+    ActualWidth := WinRect.Right - WinRect.Left;
+    ActualHeight := WinRect.Bottom - WinRect.Top;
+    LimitWidth := LimitRect.Right - LimitRect.Left;
+    LimitHeight := LimitRect.Bottom - LimitRect.Top;
+
+    // Resize form to fit screen if needed
+    if ActualWidth > LimitWidth then
+      TForm(Self).Width := TForm(Self).Width - (ActualWidth - LimitWidth);
+    if ActualHeight > LimitHeight then
+      TForm(Self).Height := TForm(Self).Height - (ActualHeight - LimitHeight);
+
+    // Re-get window rectangle if size changed, to adjust position correctly
+    if (ActualWidth > LimitWidth) or (ActualHeight > LimitHeight) then
     begin
-      // Select full desktop for stay-on-top forms, otherwise work area only
-      if TForm(Self).FormStyle = fsSystemStayOnTop then
-        LimitRect := Screen.DesktopRect
-      else
-        LimitRect := Screen.WorkAreaRect;
+      WinRect := Default(TRect);
+      {$IFDEF WINDOWS}
+      if not GetWindowRect(TForm(Self).Handle, WinRect) then
+        Exit;
+      {$ELSE}
+      WinRect := TForm(Self).BoundsRect;
+      {$ENDIF}
+    end;
 
-      // Check if form is larger than available screen area
-      ActualWidth := WinRect.Right - WinRect.Left;
-      ActualHeight := WinRect.Bottom - WinRect.Top;
-      LimitWidth := LimitRect.Right - LimitRect.Left;
-      LimitHeight := LimitRect.Bottom - LimitRect.Top;
+    // Adjust position only if not centered by system
+    if TForm(Self).Position <> poDesktopCenter then
+    begin
+      NewLeft := TForm(Self).Left;
+      NewTop := TForm(Self).Top;
 
-      // Resize form to fit screen if needed
-      if ActualWidth > LimitWidth then
-        TForm(Self).Width := TForm(Self).Width - (ActualWidth - LimitWidth);
-      if ActualHeight > LimitHeight then
-        TForm(Self).Height := TForm(Self).Height - (ActualHeight - LimitHeight);
+      // Keep right edge inside screen
+      if WinRect.Right > LimitRect.Right then
+        NewLeft := TForm(Self).Left - (WinRect.Right - LimitRect.Right);
 
-      // Re-get window rectangle if size changed, to adjust position correctly
-      if (ActualWidth > LimitWidth) or (ActualHeight > LimitHeight) then
-      begin
-        WinRect := Default(TRect);
-        if not GetWindowRect(TForm(Self).Handle, WinRect) then
-          Exit;
-      end;
+      // Keep bottom edge inside screen
+      if WinRect.Bottom > LimitRect.Bottom then
+        NewTop := TForm(Self).Top - (WinRect.Bottom - LimitRect.Bottom);
 
-      // Adjust position only if not centered by system
-      if TForm(Self).Position <> poDesktopCenter then
-      begin
-        NewLeft := TForm(Self).Left;
-        NewTop := TForm(Self).Top;
+      // Keep left edge inside screen
+      if NewLeft < LimitRect.Left then
+        NewLeft := LimitRect.Left;
 
-        // Keep right edge inside screen
-        if WinRect.Right > LimitRect.Right then
-          NewLeft := TForm(Self).Left - (WinRect.Right - LimitRect.Right);
+      // Keep top edge inside screen
+      if NewTop < LimitRect.Top then
+        NewTop := LimitRect.Top;
 
-        // Keep bottom edge inside screen
-        if WinRect.Bottom > LimitRect.Bottom then
-          NewTop := TForm(Self).Top - (WinRect.Bottom - LimitRect.Bottom);
-
-        // Keep left edge inside screen
-        if NewLeft < LimitRect.Left then
-          NewLeft := LimitRect.Left;
-
-        // Keep top edge inside screen
-        if NewTop < LimitRect.Top then
-          NewTop := LimitRect.Top;
-
-        TForm(Self).Left := NewLeft;
-        TForm(Self).Top := NewTop;
-      end;
+      TForm(Self).Left := NewLeft;
+      TForm(Self).Top := NewTop;
     end;
   end;
 end;
